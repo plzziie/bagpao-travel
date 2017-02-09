ca3a1a434247ddcabf3eb58b9931ef7e1df3784c
DESKTOP-F6IM1D0\Ami <oparasit38@gmail.com>
2017-02-09 11:50:09 +0700

    new code

--- server/APIcontactus.js
+++ server/APIcontactus.js
@@ -4,26 +4,32 @@ var express = require('express');
 var app = express();
 let mongo = require('mongodb').MongoClient;
 let connection = 'mongodb://localhost:27017/bagpaotravel';
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
+app.post('/', (req, res) => {
 
-app.get('/:id', (req, res) => {
-
-	if(req.params.id === "" && req.params.id === 0)
+	if(req.body.no === "" || req.body.no === "0")
 	{
 		res.send('please input data!!');
 	}
 	else
 	{
+		console.log(req.body.id);
 		mongo.connect(connection, (error, database) => {
 		database
 		.collection('contactus')
-		.insert({ no:`${no}`,
-			firstname:`${firstname}`,
-			lastname:`${lastname}`,
-			subject:`${subject}`,
-			email:`${email}` ,
-			message :`${message}`});
-   		 });
+		.insert({ 
+			no:`${req.body.no}`,
+			name:`${req.body.name}`,
+			subject:`${req.body.subject}`,
+			email:`${req.body.email}` ,
+			message :`${req.body.message}` 
+			});
+   		 }); 
 
 		res.send('success !!');
 	}
