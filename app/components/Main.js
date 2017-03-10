1e48d04a854a43e1c45d561c5f37f9c0bbf49489
arparpa <arpa.assawa@gmail.com>
2017-03-10 13:55:48 +0700

    footer

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react'
 import Navbar from '../components/Navbar'
 import styles from '../styles'
 
+
 class Main extends Component {
   render() {
     return (
       <div>
       	<Navbar/>{this.props.children}
+
+          <div className="container-fluid footer text-center">
+            <h5>Information Engineering</h5>
+          </div>
+
       </div>
     )
   }
