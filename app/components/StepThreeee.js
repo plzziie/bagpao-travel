6bd6d7f488a71233de4eb947927c2688f0022ca3
plzziie <plz.brow_niz@hotmail.com>
2017-04-06 15:06:27 +0700

    stepthree

--- app/components/StepThreeee.js
+++ app/components/StepThreeee.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {PropTypes} from 'react'
 import ReactDOM, {findDOMNode} from 'react-dom';
 import styles from '../styles'
+import style from '../styles/step.css'
 
 var StepThreeee = React.createClass ({
 
@@ -21,25 +22,129 @@ var StepThreeee = React.createClass ({
 
   render: function () {
     return(
-
+      <div>
       <div className="container-fluid">
-
       <ul className = "stepone nav nav-pills nav-justified">
         <li><a href = "/stepone">Step 1</a></li>
         <li><a href = "/steptwo">Step 2</a></li>
         <li className="active"><a href = "/stepthree">Step 3</a></li>
         <li><a href = "stepfour">Step 4</a></li>
       </ul>
-
-
-      <h2>Drag and Drop</h2>
-      <p>Drag the image back and forth between the two div elements.</p>
-
-      <div style = {styles.dnd} onDrop = {this.drop} onDragOver = {this.allowDrop}>
-        <img src = "./app/img/chiangdao.jpg" draggable = "true" onDragStart = {this.drag} id = "drag1" width = "180" height = "90"/>
       </div>
 
-      <div style = {styles.dnd} onDrop = {this.drop} onDragOver = {this.allowDrop}></div>
+      <div className="container">
+
+        <div className="stepthree_day">
+          <div className="row scrollmenu">
+            <div className="planbox" onDrop = {this.drop} onDragOver = {this.allowDrop}>
+                 <div className="day">Day 1</div>
+                 <div className="dragbox"><div className="col-md-3">
+                   <img className = "img-circle" src = "app/img/chiangmai.jpg" alt = "doisuthep" width="50" height="50"/></div>
+                   <div className="col-md-8"><h5>Doi Suthep-Pui National Park</h5> <h6>Chiangmai</h6></div>
+                 </div>
+            </div>
+            <div className="planbox" onDrop = {this.drop} onDragOver = {this.allowDrop}>
+                 <div className="day">Day 2</div>
+                 <div className="dragbox"><div className="col-md-3">
+                   <img className = "img-circle" src = "app/img/chiangmai.jpg" alt = "doisuthep" width="50" height="50"/></div>
+                   <div className="col-md-8"><h5>Doi Suthep-Pui National Park</h5> <h6>Chiangmai</h6></div>
+                 </div>
+            </div>
+            <div className="planbox" onDrop = {this.drop} onDragOver = {this.allowDrop}>
+                 <div className="day">Day 3</div>
+                 <div className="dragbox"><div className="col-md-3">
+                   <img className = "img-circle" src = "app/img/chiangmai.jpg" alt = "doisuthep" width="50" height="50"/></div>
+                   <div className="col-md-8"><h5>Doi Suthep-Pui National Park</h5> <h6>Chiangmai</h6></div>
+                 </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="stepthree_select">
+          <div className="where">Where you want to go?</div>
+
+                 <ul className = "nav nav-pills nav-justified ">
+                   <li className = "active"><a href="#place" data-toggle = "tab">Place</a></li>
+                   <li><a href="#restaurant" data-toggle = "tab">Restaurant</a></li>
+                   <li><a href="#accommodation" data-toggle = "tab">Accommodation</a></li>
+                 </ul>
+
+                <div className = "tab-content">
+                  <div id = "place" className = "tab-pane fade in active">
+                    <div className="between">
+                       <div className="form">
+                       <input type = "text"  className = "form-control " placeholder = "search"/>
+                       </div>
+                       <div className="col-md-3 col-xs-4">
+                       <button type = "submit" className = "buttonplan form-control">search</button>
+                      </div>
+                    </div>
+
+                    <ul className="searchbox">
+                      <li className="dragbox" draggable = "true" onDragStart = {this.drag} id = "dd">
+                        <div className="col-md-3"><img className = "img-circle" src = "app/img/chiangmai.jpg" alt = "doisuthep"
+                        width="50" height="50"/></div><div className="col-md-8"><h5>Doi Suthep-Pui National Park</h5> <h6>Chiangmai</h6></div>
+                      </li>
+                      <li className="dragbox" draggable = "true" onDragStart = {this.drag} id = "gg">
+                        <div className="col-md-3"><img className = "img-circle" src = "app/img/chiangdao.jpg" alt = "doisuthep"
+                        width="50" height="50"/></div><div className="col-md-8"><h5>Chiangdao</h5> <h6>Chiangmai</h6></div>
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div id = "restaurant" className = "tab-pane fade">
+                    <div className="between">
+                      <div className="form">
+                      <input type = "text"  className = "form-control " placeholder = "search"/>
+                      </div>
+                      <div className="col-md-3 col-xs-4">
+                      <button type = "submit" className = "buttonplan form-control">search</button>
+                      </div>
+                    </div>
+
+                    <ul className="searchbox">
+                      <li className="dragbox">
+                        <div className="col-md-3"><img className = "img-circle" src = "app/img/chiangmai.jpg" alt = "doisuthep"
+                        width="50" height="50"/></div><div className="col-md-8"><h5>Doi Suthep-Pui National Park</h5> <h6>Chiangmai</h6></div>
+                      </li>
+                      <li className="dragbox">
+                        <div className="col-md-3"><img className = "img-circle" src = "app/img/chiangmai.jpg" alt = "doisuthep"
+                        width="50" height="50"/></div><div className="col-md-8"><h5>Doi Suthep-Pui National Park</h5> <h6>Chiangmai</h6></div>
+                      </li>
+                    </ul>
+                  </div>
+
+                <div id = "accommodation" className = "tab-pane fade">
+                  <div className="between">
+                    <div className="form">
+                    <input type = "text"  className = "form-control " placeholder = "search"/>
+                    </div>
+                    <div className="col-md-3 col-xs-4">
+                    <button type = "submit" className = "buttonplan form-control">search</button>
+                    </div>
+                  </div>
+
+                  <ul className="searchbox">
+                    <li className="dragbox">
+                      <div className="col-md-3"><img className = "img-circle" src = "app/img/chiangmai.jpg" alt = "doisuthep"
+                      width="50" height="50"/></div><div className="col-md-8"><h5>Doi Suthep-Pui National Park</h5> <h6>Chiangmai</h6></div>
+                    </li>
+                    <li className="dragbox">
+                      <div className="col-md-3"><img className = "img-circle" src = "app/img/chiangmai.jpg" alt = "doisuthep"
+                      width="50" height="50"/></div><div className="col-md-8"><h5>Doi Suthep-Pui National Park</h5> <h6>Chiangmai</h6></div>
+                    </li>
+                  </ul>
+                </div>
+
+              </div>
+            </div>
+
+            <div className="col-md-offset-4 col-sm-offset-4 col-xs-offset-4 col-md-4 col-sm-4 col-xs-4 step-bottom">
+             <button  type = "submit" className = "button form-control">
+               Next
+             </button>
+            </div>
+      </div>
     </div>
     )
 	}
