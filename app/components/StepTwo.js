4cc650c96be48a4ec7b0f5e334cc758207740385
arparpa <arpa.assawa@gmail.com>
2017-04-14 07:16:36 +0700

    step2 deco

--- app/components/StepTwo.js
+++ app/components/StepTwo.js
@@ -18,17 +18,19 @@ function StepTwo (props) {
         <form onSubmit = {props.onSubmitTrip}>
           <div className="container">
 
-            <div className = "col-md-5 col-sm-offset-0 col-sm-6">
+            <div className = "col-md-offset-2 col-md-9 step-bottom steptwosub">
             <h2>Departure</h2>
               <div className = "col-xs-10 ">
-              <p className = "form-control-static">{props.origin}</p>
-              <select onChange = {props.onUpdateVehiclesD} >
+              <p className = "form-control-static">
+                <b>From</b> {props.origin}
+                <b> To</b> {props.destination}</p>
+              <select className="select btn-info" onChange = {props.onUpdateVehiclesD} >
                   <option value>Transportation</option>
                   <option value = "bus">Bus</option>
                   <option value = "train">Train</option>
                   <option value = "plane">Plane</option>
-               </select>
-               <select onChange = {props.onUpdateDepart}> <option value>Routes</option> {props.transportation.map((val, index) => {
+               </select>&nbsp;
+               <select className="select" onChange = {props.onUpdateDepart}> <option value>Routes</option> {props.transportation.map((val, index) => {
                     return (val.type == props.vehiclesD)
                     ? (val.origin == props.origin)
                       ? <option value = {val.name} key = {index}>From {val.stationstart} ({val.depart}) To {val.stationend} ({val.arrive})</option>
@@ -36,21 +38,23 @@ function StepTwo (props) {
                     : null
                   })}
                   </select>
-              <p className = "form-control-static">{props.destination}</p>
+
               </div>
             </div>
 
-            <div className = "col-md-5 col-sm-offset-0 col-sm-6">
+            <div className = "col-md-offset-2 col-md-9 steptwosub">
             <h2>Return</h2>
               <div className = "col-xs-10">
-              <p className = "form-control-static">{props.destination}</p>
-              <select onChange = {props.onUpdateVehiclesR}>
+              <p className = "form-control-static">
+                <b>From</b> {props.destination}
+                <b> To</b> {props.origin}</p>
+              <select className="select btn-info" onChange = {props.onUpdateVehiclesR}>
                   <option value>Transportation</option>
                   <option value = "bus">Bus</option>
                   <option value = "train">Train</option>
                   <option value = "plane">Plane</option>
-               </select>
-               <select onChange = {props.onUpdateReturn}> <option value>Routes</option> {props.transportation.map((val, index) => {
+               </select>&nbsp;
+               <select className="select" onChange = {props.onUpdateReturn}> <option value>Routes</option> {props.transportation.map((val, index) => {
                     return (val.type == props.vehiclesR)
                     ? (val.origin == props.destination)
                       ? <option value = {val.name} key = {index}>From {val.stationstart} ({val.depart}) To {val.stationend} ({val.arrive})</option>
@@ -58,7 +62,6 @@ function StepTwo (props) {
                     : null
                   })}
               </select>
-              <p className = "form-control-static">{props.origin}</p>
               </div>
             </div>
 
