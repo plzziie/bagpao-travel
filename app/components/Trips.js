07d3f2d0c3428a1db7a38c11546194d1dd7aca45
arparpa <arpa.assawa@gmail.com>
2017-04-19 02:49:07 +0700

    like fav share

--- app/components/Trips.js
+++ app/components/Trips.js
@@ -48,22 +48,32 @@ function Trips (props) {
       <div className = "col-md-offset-1 col-md-10 transparentBg">
       {props.searching
         ? props.found ? props.trips.map((val, index) => {
-                        return <div key = {index} className = "col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0 col-sm-4 col-sm-offset-0  photo">
-                                  <img className = "img" src = {val.picture} alt = {val.name} width="376" height="251"/>
-                                  <div className="bottomleft1">{val.name.toUpperCase()}</div>
-                                  <div className="box"></div>
-                                  <div className="bottomleft2">by {val.creator.toUpperCase()}</div>
-                                </div>
+          return <div key = {index}><div className = "col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0 col-sm-4 col-sm-offset-0 photo">
+                    <a href="trips-details" ><img className = "img favgap" src = {val.picture} alt = {val.name} width="376" height="251"/>
+                    <div className="bottomlefttrip1">{val.name.toUpperCase()}</div>
+                    <div className="boxtrip"></div>
+                    <div className="bottomlefttrip2">by {val.creator.toUpperCase()}</div></a>
+                    <input type="checkbox" id="like" className="checklike"/><label className="like">Like</label>
+                    <input type="checkbox" id="fav"  className="cb_position"/>&nbsp;&nbsp;<label className="heart"></label>
+                    <label className="col-md-offset-8 text-right"><div className="share">share</div></label>
+
+                  </div>
+                </div>
                       })
                     : <h4>{props.err}</h4>
 
       : props.show.map((val, index) => {
-                   return <div key = {index} className = "col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0 col-sm-4 col-sm-offset-0  photo">
-                        <img className = "img" src = {val.picture} alt = {val.name} width="376" height="251"/>
-                       <div className="bottomleft1">{val.name.toUpperCase()}</div>
-                       <div className="box"></div>
-                       <div className="bottomleft2">by {val.creator.toUpperCase()}</div>
-                     </div>
+        return <div key = {index}><div className = "col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0 col-sm-4 col-sm-offset-0 photo">
+                  <a href="trips-details" ><img className = "img favgap" src = {val.picture} alt = {val.name} width="376" height="251"/>
+                  <div className="bottomlefttrip1">{val.name.toUpperCase()}</div>
+                  <div className="boxtrip"></div>
+                  <div className="bottomlefttrip2">by {val.creator.toUpperCase()}</div></a>
+                  <input type="checkbox" id="like" className="checklike"/><label className="like">Like</label>
+                  <input type="checkbox" id="fav"  className="cb_position"/>&nbsp;&nbsp;<label className="heart"></label>
+                  <label className="col-md-offset-8 text-right"><div className="share">share</div></label>
+
+                </div>
+              </div>
                   })
       }
     </div>
