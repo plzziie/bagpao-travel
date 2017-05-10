071537c4d1f74b64b8c7b90719fd654d4b80225e
arparpa <arpa.assawa@gmail.com>
2017-05-10 15:36:35 +0700

    category

---
+++ app/components/Forget.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+import {PropTypes} from 'react'
+import validator from 'bootstrap-validator'
+import styles from '../styles'
+
+function Forget (props) {
+  console.log(props)
+    return (
+      <div className="container-fluid">
+      <div className = "jumbotron col-xs-8 col-xs-offset-2" style = {styles.transparentBg}>
+
+        <form data-toggle = "validator" role = "form" >
+          <div className = "form-group">
+            <input
+              className = 'form-control'
+              placeholder = 'username'
+
+              type = 'text'
+              data-error = "Please enter your username"
+              required />
+            <div className = "help-block with-errors"></div>
+          </div>
+          <div className = "form-group">
+            <input
+              className = 'form-control'
+              placeholder = 'password'
+
+              type = 'password'
+              data-error = "Please enter your password"
+              required />
+            <div className = "help-block with-errors"></div>
+          </div>
+          <div className = "form-group col-xs-4 col-xs-offset-4">
+            <button
+              className = "btn btn-block btn-success"
+              type = "submit">
+              Login
+            </button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+    )
+  }
+
+
+
+export default Forget
